0d6089dfd5597cf7954386f40dbae7fa6b40070c
EddieFreedome <eddie.freed@tutanota.com>
2021-10-20 17:37:45 +0200

    added js + small corrections and comments

---
+++ script.js
@@ -0,0 +1,132 @@
+/*
+- avere una select che permette di scegliere fra 3 difficoltà
+  - in base alla difficoltà scelta, dobbiamo disegnare PIU' o MENO celle / quadratini
+- vicino al select, dobbiamo creare un pulsante.
+  - al click: (event listener)
+    - Leggiamo il livello scelto dall'utente
+    - In base al livello scelto, andiamo a generare e stampare in html la nostra griglia
+      - questa dovrà tenere conto della scelta dell'utente
+        per stampare il numero corretto di celle.
+    
+- una volta creata la griglia con le singole celle, al click su ogni cella,
+  dobbiamo cambiare il suo colore.
+*/
+
+//cliccando il pulsante play subentrano altre variabili: 
+// un livello che verra' generato in base alla scelta della difficolta', il numero delle Celle, e le bombe
+
+
+const difficolta = document.getElementById("difficulties")
+const playButton = document.getElementById("play")
+const squareContainer = document.querySelector(".square-container")
+
+//dopo aver flaggato il livello scelto, con il click facciamo leggere il valore che assume il livello
+//scelta livello
+playButton.addEventListener(click, function(){
+const level = difficolta.value; //value restituisce qualsiasi tipo sia di valore della variabile
+
+//una volta letta la difficolta' scelta dovra' essere creato il numero di celle corrispondenti alla difficolta'
+/*celle da creare*/ /*=*/ /*generatorecelletotali che sceglie in base alla difficolta'*/
+celleTotali = generaCelle(level); //ho asssunto il valore sottoforma di variabile del numero delle celle
+//ho bisogno ora di generare la griglia corrispondente al numero di celle
+
+})
+
+const square = document.createElement("div")
+
+for (let i = 1; i <= 100 ; i++) {
+    //genero col
+    //attacco col
+    const square = document.createElement("div")
+    square.classList.add ("my-col")
+    if (i % 3 === 0 && i % 5 === 0)  {
+        square.classList.add("x3-5")
+        square.innerHTML = "FizzBuzz"
+    } else if (i % 5 === 0)  {
+        square.classList.add("x5")
+        square.innerHTML = "Buzz"
+    } else if (i % 3 === 0)  {
+        square.classList.add("x3")
+        square.innerHTML = "Fizz"
+    } else {
+        square.classList.add("std")
+        square.innerHTML = i
+    }
+
+containerGrid.append(square) //aggiunge alla pagina il mio html dopo averlo creato
+}
+
+//Funzioni: 
+function generaCelle (level){
+    let result; //creo variabile casistiche da restituire
+
+  switch (parseInt(level)) {
+    case 1: //easy 100 celle
+      result = 100;
+      break;
+    case 2: //medium 81 celle
+      result = 81;
+      break;
+    case 3: //hard 49 celle
+      result = 49;
+      break;
+    }  
+    
+    return result;
+}
+
+function generaGriglia(celleTotali) {
+    // Resetto tutto il contenuto del boardcontainer
+    boardContainer.innerHTML = "";
+    //calcolo la radice quadrata del numero di celle totali, visivamente si
+    //traduce in un'area distribuita di 10 celle per riga x 10 celle per riga
+    const perRowCells = Math.sqrt(celleTotali);
+    //posto che ci son 10 celle per riga, quali dimensioni dovranno avere?
+    //1 cella = 100% / numeroCelle per riga!
+    const cellSize = 100 / perRowCells;
+    //trovato il numero di celle per riga e la dimensione che devono occupare,
+    // Creo un ciclo in base al numero di celle da creare
+    for (let i = 0; i < celleTotali; i++) {
+      // genero una singola cella per volta fino al numero indicato dalla variabile
+      const cell = generateSingleCell(i, cellSize); //**
+  
+      // Aggiungo la cella al boardContainer
+      //boardContainer.innerHTML += cell;
+      boardContainer.append(cell);
+    }
+//append.(cella) dopo aver creato tutte le celle div con create element
+}
+
+function generaCellaSingola(){ //**
+    //anziche' creare una variabile Cella e scrivergli con il template `div class, style ecc..`
+    //creo una cella "div" con createElement e la andro' 
+    //ad appendere e stilizzare poi aggiungendo classi nel corso della funzione.
+    const cella = document.createElement("div");
+    //aggiungo lo stile css alla cella aggiungendole la classe corrispondente in css
+    //senza dimensioni! la larghezza e l'altezza vanno generate con valori
+    //non statici in base alla richiesta: ...style inline?
+    cella.classList.add("square"); //aggiunto classe stile .square dal css
+    //servono dimensioni ora con style inline che cambia in base alla cellSize stabilita prima
+    //aggiungo il % come stringa per "falsare" la lettura del valore numerico e ingannarlo a leggerlo come percentuale
+    cella.style.width = cellSize + "%";
+    cella.style.height = cellSize + "%";
+    
+
+    //----- non ho capito ---/
+    // cell.numeroCella = i + 1;
+
+    // cell.addEventListener("click", onSingleCellClick);
+    // // cell.addEventListener("click", () => onSingleCellClick(i + 1, cell));
+    // /*   cell.addEventListener("click", function () {
+    //     this.classList.toggle("clicked");
+    
+    //     console.log("clickata cella #" + (i + 1));
+    //   }); */
+  
+  
+    // cell.textContent = i + 1;
+  
+    // return cell;
+  }
+
+}
